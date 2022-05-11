dee787e6233508563560054466794d4cfb8d065b
Sleta Kirill <sleta.kirill2004@gmail.com>
2022-05-11 17:36:27 +0300

    hw-14

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // const el = document.querySelector('.tab-container')
 // const tab = new Tab(el);
 
+const el = document.querySelector('.container');
+const tab = new Tab(el)
+
 
 
 
--- tabs/tab.js
+++ tabs/tab.js
@@ -35,3 +35,55 @@
 //     }
 // }
 
+
+class Tab{
+    static CLASSES = {
+       active: 'title-active',
+       title_cont: 'title-container',
+       show: 'show',
+        title: 'item-title',
+        body: 'item-body'
+    }
+    constructor(el){
+        this.init(el)
+    }
+
+    init(el){
+        this.setElement(el)
+        this.initialClassSet(el)
+        el.children[0].addEventListener('click', this.onTab)
+    }
+
+    setElement(el){
+        this.titles = [...el.children[0].children];
+        this.bodys = [...el.children[1].children];
+        this.activeIndex = 0;
+    }
+    initialClassSet(el){
+        this.setActiveClasses(this.titles, Tab.CLASSES.active, Tab.CLASSES.title)
+        this.setActiveClasses(this.bodys, Tab.CLASSES.show, Tab.CLASSES.body)
+        el.children[0].classList.add(Tab.CLASSES.title_cont)
+    }
+
+    onTab = (e) =>{
+        console.log(e.target)
+    }
+
+    showContent(){
+
+    }
+
+    hideContent(){
+
+    }
+
+    setActiveClasses(elements, activeClass, comonClass){
+        elements.forEach((e, i) =>{
+            e.classList.add(comonClass);
+            if(i === this.activeIndex){
+                e.classList.add(activeClass)
+            }
+        })
+    }
+
+}
