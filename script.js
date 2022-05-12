0aac3e893d15d9f5d2a704eae9f9725389f068ae
Sleta Kirill <sleta.kirill2004@gmail.com>
2022-05-12 10:41:38 +0300

    hw-13

--- script.js
+++ script.js
@@ -1,58 +1,119 @@
-class Student {
-    constructor(name, mark){
-      this.studentName = name;
-        this.studentMarks = mark
-    }
+
+// class Student {
+//     constructor(name, mark){
+//       this.studentName = name;
+//         this.studentMarks = mark
+//     }
    
-}
+// }
 
 
-class Group {
-    constructor(){
-        this.students = []; 
+// class Group {
+//     constructor(){
+//         this.students = [];  
+//     }
+//     addStudent(student){
+//         return this.students.push(student)
+//     }
+//     getAverageMark(){ 
+//          let studentNotes =  this.students.map((e) => {
+//                 const marks =
+//                  e.students.reduce(
+//                      (acc, mar)=>
+//                 (acc+= mar.studentNotes.reduce((a,m)=>(a+=m),0)/e.studentMarks.length),
+//                 0)/ studentNotes.length
+// console.log(marks)
+    
+
+//             // return e.studentMarks.reduce((acc, el) => {
+//             //     return(acc += el)
+//             // },0) /  e.studentMarks.length
+//             // })
+
+//             // return studentNotes.reduce((acc, el) =>{
+//             //     return acc+=el
+//             //  },0) / studentNotes.length
+//     })
+// }
+// }           
+
+// const feGroup = new Group();
+// const firstStudent = new Student('John Doe', [10, 102, 0]);
+
+// feGroup.addStudent(new Student('Alex Smith', [10, 9, 8]));
+// feGroup.addStudent( new Student('John Doe', [10, 10, 5, 10]));
+// feGroup.addStudent(new Student('Bob Johnson', [9, 10, 10, 8]));
+
+// console.log(feGroup.students);
+
+// const averageMark = feGroup.getAverageMark()
+
+// console.log(averageMark);
+
+
+const stu = [
+    {
+      marks: [1, 2, 3,4,5],
+    },
+    {
+      marks: [1, 2, 3],
+    },
+  ];
+  
+  const averageM =
+    stu.reduce(
+      (acc, st) =>
+        (acc += st.marks.reduce((a, m) => (a += m), 0) / st.marks.length),
+      0
+    ) / stu.length;
+  
+    // console.log(averageM)
+
+
+    class Student {
+        constructor(name, mark){
+          this.studentName = name;
+            this.studentMarks = mark
+        }
+       
     }
-    addStudent(student){
-        return this.students.push(student)
+    
+    
+    class Group {
+        constructor(){
+            this.students = [];  
+        }
+        addStudent(student){
+            return this.students.push(student)
+        }
+        getAverageMark(){ 
+             
+        
+    
+                // return e.studentMarks.reduce((acc, el) => {
+                //     return(acc += el)
+                // },0) /  e.studentMarks.length
+                // })
+    
+                // return studentNotes.reduce((acc, el) =>{
+                //     return acc+=el
+                //  },0) / studentNotes.length
+        }
     }
-    getAverageMark(){
-        const marks = this.students.map((e) => {
-            const mark = e;
-          return  this.studentMarks.reduce((acc, mark) => (acc += mark),0) / studentMarks.length
-            
-        })
-
-
-        // const marks = student.studentMarks
-        // console.log(marks);
-
-
-        // let sum = 0
-        // let count = 0
-        // for(let student of this.students){
-        //     const marks = student.studentMarks
-        //     console.log(marks)
-
-        //     for(let mark of marks){
-        //         sum += mark
-        //         count++
-        //     }           
-        // }
-        // return sum / count 
-    }
-}
-
-const feGroup = new Group();
-const firstStudent = new Student('John Doe', [10, 102, 0]);
-
-feGroup.addStudent(new Student('Alex Smith', [10, 9, 8]));
-feGroup.addStudent( new Student('John Doe', [10, 10, 5, 10]));
-feGroup.addStudent(new Student('Bob Johnson', [9, 10, 10, 8]));
-
-console.log(feGroup.students);
-
-const averageMark = feGroup.getAverageMark()
-
-console.log(averageMark);
-
+              
+    
+    const feGroup = new Group();
+    const firstStudent = new Student('John Doe', [10, 102, 0]);
+    
+    feGroup.addStudent(new Student('Alex Smith', [10, 9, 8]));
+    feGroup.addStudent( new Student('John Doe', [10, 10, 5, 10]));
+    feGroup.addStudent(new Student('Bob Johnson', [9, 10, 10, 8]));
+    
+    console.log(feGroup.students);
+    
+    const averageMark = feGroup.getAverageMark()
+    
+    console.log(averageMark);
+    
 
 
