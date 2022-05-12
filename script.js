8cec9df7505fce3ed34fe0b70e76c647ecb56021
Sleta Kirill <sleta.kirill2004@gmail.com>
2022-05-12 14:35:37 +0300

    hw-15

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
 
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
 
 
