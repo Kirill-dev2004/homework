0b09133cdd5dc3336b295de3d7d5af2e15a6c606
Sleta Kirill <sleta.kirill2004@gmail.com>
2022-05-13 11:05:26 +0300

    hw-15

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const menuBtn = document.getElementById('menu-btn');
+// const menuBtn = document.getElementById('menu-btn');
+// const menuE = document.getElementById('menu');
+
+
+// menuBtn.addEventListener('click', onClick)
+
+// function onClick(){
+// 	menuBtn.classList.toggle('active');
+// 	menuE.classList.toggle('active');
+// }
+
+// function closestMenu(){
+
+//   const menu = [...menuE.children]
+//   menu.forEach((div)=>{
+//     div.addEventListener('click', ()=>{
+//       menuBtn.classList.toggle('active');
+//       menuE.classList.toggle('active')
+//     })
+//   })
+// }
+// closestMenu()
+
+const btnE = document.getElementById('menu-btn');
 const menuE = document.getElementById('menu');
-
-
-menuBtn.addEventListener('click', onClick)
-
-function onClick(){
-	menuBtn.classList.toggle('active');
-	menuE.classList.toggle('active');
-}
-
-function closestMenu(){
-
-  const menu = [...menuE.children]
-  menu.forEach((div)=>{
-    div.addEventListener('click', ()=>{
-      menuBtn.classList.toggle('active');
-      menuE.classList.toggle('active')
-    })
-  })
-}
-closestMenu()
+const tab = new Burger(btnE, menuE)
