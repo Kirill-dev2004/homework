06d9d55d4a8ea04e1f7f144deaab16949472f494
Sleta Kirill <sleta.kirill2004@gmail.com>
2022-05-13 15:16:48 +0300

    complieted hw-15

--- burger.js
+++ burger.js
@@ -1,3 +1,34 @@
 class Burger{
+    static CLASSES = {
+        burger: 'menu-btn',
+        menu: 'menu',
+        active: 'active',
+    }
     
-}
+    constructor(contE){
+        this.el = contE;
+        this.el.addEventListener('click', this.onClickBurg.bind(this))
+        this.items = [...contE.children]
+        this.setClassChildren()
+    }
+
+    setClassChildren(){
+        const [burger, menu] = this.el.children;
+        burger.classList.add(Burger.CLASSES.burger)
+        menu.classList.add(Burger.CLASSES.menu)
+    }
+
+    onClickBurg(event){
+        const target = event.target;
+        const [burger, menu] = this.el.children
+        const menuChild = [...menu.children]   /////тут хотел добраться до дивов меню, чтобы по клику на них закрывать все///////////
+        console.log(menuChild)
+        if(burger === target){
+            burger.classList.toggle(Burger.CLASSES.active);
+            menu.classList.toggle(Burger.CLASSES.active)
+        }else{
+            burger.classList.toggle(Burger.CLASSES.active);
+            menu.classList.toggle(Burger.CLASSES.active)
+        }
+    }
+    }
--- script.js
+++ script.js
@@ -1,26 +1,2 @@
-// const menuBtn = document.getElementById('menu-btn');
-// const menuE = document.getElementById('menu');
-
-
-// menuBtn.addEventListener('click', onClick)
-
-// function onClick(){
-// 	menuBtn.classList.toggle('active');
-// 	menuE.classList.toggle('active');
-// }
-
-// function closestMenu(){
-
-//   const menu = [...menuE.children]
-//   menu.forEach((div)=>{
-//     div.addEventListener('click', ()=>{
-//       menuBtn.classList.toggle('active');
-//       menuE.classList.toggle('active')
-//     })
-//   })
-// }
-// closestMenu()
-
-const btnE = document.getElementById('menu-btn');
-const menuE = document.getElementById('menu');
-const tab = new Burger(btnE, menuE)
+const contE = document.querySelector('.container')
+const tab = new Burger(contE)
