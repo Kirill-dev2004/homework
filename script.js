b60e7d7af60cf46030067bf13e146371af7ce0ef
Sleta Kirill <sleta.kirill2004@gmail.com>
2022-05-19 13:12:48 +0300

    no

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const inpE = document.getElementById('inp')
+const btnE = document.getElementById('btn')
+const url = ` https://api.github.com/users/`
+
+btnE.addEventListener('click', findUsers)
+
+function findUsers(){
+    const allUrl = url + inpE.value
+  const urls =  fetch(allUrl)
+    console.log(urls);
+    inpE.value = ''
+}
